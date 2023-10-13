951a28cc8c192ebc0db176394cdf010692411d89
hehehub <1105597151@qq.com>
2023-10-13 18:22:55 +0800

    Create sortbyx.h

---
+++ sortbyx.h
@@ -0,0 +1,21 @@
+#include <cstdlib>
+
+// 注意这里有函数，这个头文件不可以多次引用
+struct tower
+{
+    long long x, y;
+};
+
+int comparePoints(const void *a, const void *b)
+{
+    struct tower *pointA = (struct tower *)a;
+    struct tower *pointB = (struct tower *)b;
+
+    // 按照x坐标从小到大排序
+    return pointA->x - pointB->x;
+}
+
+// main函数里面这么操作
+// struct tower* towers = (struct tower*)malloc(n * sizeof(struct tower));// 存储所有灯塔坐标，这是创建数组，元素是二维数对
+// …………
+// qsort(towers, n, sizeof(struct tower), comparePoints);// 所有灯塔x坐标从小到大，这是qsort的用法
