046fcff91ad3b745d5141e6bcf1ee07c7dc8aea4
hehehub <1105597151@qq.com>
2023-10-15 21:20:20 +0800

    Create Stack.h

---
+++ Stack.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <math.h>
+
+template <class T>
+class Stack
+{
+    int _size;
+    int _capacity;
+    T *_elem;
+
+public:
+    Stack(int s = 0, T v = 0)
+    {
+        _capacity = fmax(DEFAULT_CAPACITY, s);
+        _elem = new T[_capacity];
+        for (_size = 0; _size < s; _elem[_size++] = v)
+            ;
+    }
+
+    void push()
+    {
+        if (_size >= _capacity)
+        {
+            expand();
+        }
+        _elem[_size++] = e;
+    }
+
+    void pop()
+    {
+        if (_size > 0)
+        {
+            _size--;
+        }
+    }
+
+    T &top() const
+    {
+        return _elem[_size - 1];
+    }
+};
