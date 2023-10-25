432ed02eed903591b7cf705bfb0504fd8fa3cd54
hehehub <1105597151@qq.com>
2023-10-25 10:32:56 +0800

    Create Queue.h

---
+++ Queue.h
@@ -0,0 +1,66 @@
+// 待修改
+#pragma once
+#include <math.h>
+
+template <class T>
+class Queue
+{
+    int _size;
+    int _capacity;
+    T *_elem;
+
+    void expand()
+    {
+        if (_size < _capacity)
+            return;
+        _capacity = (_capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : _capacity * 2;
+        T *oldElem = _elem;
+        _elem = new T[_capacity];
+        for (int i = 0; i < _size; i++)
+            _elem[i] = oldElem[i];
+        delete[] oldElem;
+    }
+
+public:
+    Queue(int s = 0, T v = 0)
+    {
+        _capacity = fmax(DEFAULT_CAPACITY, s);
+        _elem = new T[_capacity];
+        for (_size = 0; _size < s; _elem[_size++] = v)
+            ;
+    }
+
+    void push(T const &e)
+    {
+        if (_size >= _capacity)
+        {
+            expand();
+        }
+        _elem[_size++] = e;
+    }
+
+    void pop()
+    {
+        if (_size > 0)
+        {
+            _size--;
+        }
+    }
+
+    T &front()
+    {
+        return _elem[0];
+    }
+
+    int size() const
+    {
+        return _size;
+    }
+
+    bool empty()
+    {
+        return _size == 0;
+    }
+
+    ~Queue() { delete[] _elem; }
+};
