d70c2a4c92301320c2f76e4d0c4f3e27c077463e
hehehub <1105597151@qq.com>
2023-11-02 23:18:02 +0800

    .

---
+++ BinTree.h
@@ -0,0 +1,170 @@
+#pragma once
+#include "Queue.h"
+#define stature(p) ((p) ? (p)->height : -1)
+// 节点高度（宏定义封装，使得实现简便，
+// 与“空树高度为-1”的约定相统一）
+
+template <class T>
+struct BinNode
+{
+    T data;             // 数值
+    BinNode<T> *parent; // 父节点
+    BinNode<T> *lc;     // 左、右孩子
+    BinNode<T> *rc;
+    int height; // 高度
+
+    BinNode(T const &e, BinNode<T> *p = nullptr)
+        : data(e), parent(p), lc(nullptr), rc(nullptr), height(0) {}
+
+    BinNode<T> *insertAsLC(T const &e) // 将e作为当前节点的左孩子插入二叉树
+    {
+        return lc = new BinNode(e, this);
+    }
+
+    BinNode<T> *insertAsRC(T const &e) // 作为当前节点的右孩子插入新节点
+    {
+        return rc = new BinNode(this, e);
+    }
+
+    BinNode<T> *succ() // 当前节点直接后继
+    {
+        return nullptr;
+    }
+
+    bool HasLChild()
+    {
+        return node->lc != nullptr;
+    }
+
+    bool HasRChild()
+    {
+        return node->rc != nullptr;
+    }
+
+    template <class VST>
+    void travLevel(VST &visit) // 二叉树层次遍历算法
+    {
+        Queue<BinNode<T> *> Q; // 辅助队列
+        Q.push(this);          // 根节点入队
+        while (!Q.empty())     // 在队列再次变空之前，反复迭代
+        {
+            BinNode<T> *x = Q.pop();
+            visit(x->data); // 取出队首节点并访问之
+            if (x->HasLChild())
+                Q.push(x->lc); // 左孩子入队
+            if (x->HasRChild())
+                Q.push(x->rc); // 右孩子入队
+        }
+    }
+
+    int size() // 以当前节点为根的子树规模
+    {
+        int s = 1; // 计入本身
+        if (lc)
+        {
+            s += lc->size(); // 递归计入左子树规模
+        }
+        if (rc)
+        {
+            s += rc->size(); // 递归计入右子树规模
+        }
+        return s;
+    }
+
+    int getHeight()
+    {
+        if (!this)
+            return -1;
+        else
+            return 1 + max(height(lc), height(rc));
+    }
+};
+
+template <class T, class VST>
+class BinTree
+{
+    int _size;         // 规模
+    BinNode<T> *_root; // 根节点
+
+    virtual int updateHeight(BinNode<T> *x) // 更新节点x高度
+    {
+        return x->height = 1 + max(stature(x->lc), stature(x->rc));
+    }
+
+    void updateHeightAbove(BinNode<T> *x) // 更新节点x及其祖先高度
+    {
+        while (x) // 从x出发，覆盖历代祖先
+        {
+            updateHeight(x);
+            x = x->parent;
+        }
+    }
+
+    static int removeAt(BinNode<T> *x) // 释放被摘除节点，并返回删除节点总数
+    {
+        if (!x)
+            return 0;                                  // 递归基：空树
+        int n = 1 + removeAt(x->lc) + removeAt(x->rc); // 递归释放左右子树
+        delete (x);
+        return n;
+    }
+
+public:
+    int size() const { return _size; } // 规模
+
+    bool empty() const { return !_root; } // 判空
+
+    BinNode<T> *root() const { return _root; } // 树根
+
+    BinNode<T> *insertAsLC(BinNode<T> *x, T const &e) // e插入为x的左孩子
+    {
+        _size++;
+        x->insertAsLC(e);
+        updateHeightAbove(x);
+        return x->lc;
+    }
+
+    void travPre(BinNode<T> *x, VST &visit) // 前序遍历
+    {
+        visit(x->data);
+        travPre(x->lc, visit);
+        travPre(x->rc, visit);
+    }
+
+    void travIn_R(BinNode<T> *x, VST &visit) // 中序遍历
+    {
+        travIn_R(x->lc, visit);
+        visit(x->data);
+        travIn_R(x->rc, visit);
+    }
+
+    void travPost_R(BinNode<T> *x, VST &visit) // 后序遍历
+    {
+        travPost_R(x->lc, visit);
+        travPost_R(x->rc, visit);
+        visit(x->data);
+    }
+
+    int height(BinNode<T> *x)
+    {
+        if (!x)
+            return -1;
+        else
+            return 1 + max(height(x->lc), height(x->rc));
+    }
+
+    int remove(BinNode<T> *x) // 删除子树x，更新规模，返回删除节点总数
+    {
+        FromParentTo(*x) = nullptr;   // 切断来自父节点的指针
+        updateHeightAbove(x->parent); // 更新祖先高度
+        int n = removeAt(x);
+        _size -= n;
+        return n;
+    }
+
+    ~BinTree() // 析构函数
+    {
+        if (0 < _size)
+            remove(_root);
+    }
+};
--- Queue.h
+++ Queue.h
@@ -1,66 +1,77 @@
-// 待修改
 #pragma once
-#include <math.h>
+#include <math.h> // std::max
 
 template <class T>
 class Queue
 {
-    int _size;
-    int _capacity;
-    T *_elem;
+private:
+    static const int DEFAULT_CAPACITY = 10; // 默认的容量
+    int _front;                             // 队列头部索引
+    int _size;                              // 队列大小
+    int _capacity;                          // 队列容量
+    T *_elem;                               // 动态数组
 
+    // 扩展队列容量
     void expand()
     {
         if (_size < _capacity)
             return;
-        _capacity = (_capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : _capacity * 2;
+        _capacity = fmax(DEFAULT_CAPACITY, _capacity * 2);
         T *oldElem = _elem;
         _elem = new T[_capacity];
-        for (int i = 0; i < _size; i++)
-            _elem[i] = oldElem[i];
+        for (int i = 0; i < _size; ++i)
+        {
+            _elem[i] = oldElem[(_front + i) % _size]; // 从 _front 开始复制
+        }
+        _front = 0; // 重置 _front
         delete[] oldElem;
     }
 
 public:
-    Queue(int s = 0, T v = 0)
+    // 构造函数
+    Queue(int cap = DEFAULT_CAPACITY) : _front(0), _size(0), _capacity(cap)
     {
-        _capacity = fmax(DEFAULT_CAPACITY, s);
         _elem = new T[_capacity];
-        for (_size = 0; _size < s; _elem[_size++] = v)
-            ;
     }
 
-    void push(T const &e)
+    // 入队
+    void push(const T &e)
     {
-        if (_size >= _capacity)
-        {
-            expand();
-        }
-        _elem[_size++] = e;
+        expand(); // 确保有足够的空间
+        _elem[(_front + _size) % _capacity] = e;
+        ++_size;
     }
 
+    // 出队
     void pop()
     {
-        if (_size > 0)
-        {
-            _size--;
-        }
+        if (_size == 0)
+            return; // 防止空队列操作
+        _front = (_front + 1) % _capacity;
+        --_size;
     }
 
+    // 获取队列头部元素
     T &front()
     {
-        return _elem[0];
+        return _elem[_front];
     }
 
+    // 获取队列元素数量
     int size() const
     {
         return _size;
     }
 
-    bool empty()
+    // 检查队列是否为空
+    bool empty() const
     {
         return _size == 0;
     }
 
-    ~Queue() { delete[] _elem; }
+    // 析构函数
+    ~Queue()
+    {
+        delete[] _elem;
+    }
 };
