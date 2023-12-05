55e905b99e5b5fa8adaaa17f2b339e0a536045ac
hehehub <1105597151@qq.com>
2023-12-06 00:49:36 +0800

    Update Queue.h

--- Queue.h
+++ Queue.h
@@ -1,99 +1,78 @@
 #pragma once
-#include <math.h> // std::max
+
+template <class T>
+struct Node
+{
+    T data;
+    Node<T> *next;
+};
 
 template <class T>
 class Queue
 {
 private:
-    int _front;    // 队列头部索引
-    int _size;     // 队列大小
-    int _capacity; // 队列容量
-    T *_elem;      // 动态数组
-
-    // 扩展队列容量
-    void expand()
-    {
-        if (_size < _capacity)
-            return;
-        _capacity = fmax(10, _capacity * 2);
-        T *oldElem = _elem;
-        _elem = new T[_capacity];
-        for (int i = 0; i < _size; ++i)
-        {
-            _elem[i] = oldElem[(_front + i) % _size];
-        }
-        _front = 0; // 重置 _front
-        delete[] oldElem;
-    }
-
-    void shrink() // 有必要时缩容
-    {
-        if (_size < _capacity) // 判断是否需要缩容，这里我们假设当大小小于容量的一半时进行缩容
-        {
-            T *newElem = new T[_size];
-            for (int i = 0; i < _size; i++)
-            {
-                newElem[i] = _elem[(_front + i) % _capacity];
-            }
-
-            delete[] _elem;
-            _elem = newElem;
-            _front = 0;
-            _capacity = _size;
-        }
-    }
+    int _size;
+    Node<T> *head;
+    Node<T> *tail;
 
 public:
-    // 构造函数
-    Queue(int cap = 10) : _front(0), _size(0), _capacity(cap)
+    Queue() : _size(0)
     {
-        _elem = new T[_capacity];
+        head = new Node<T>;
+        head->next = nullptr;
+        tail = head;
     }
 
-    // 入队
-    void push(const T &e)
+    void push(const T &_data)
     {
-        expand(); // 确保有足够的空间
-        _elem[(_front + _size) % _capacity] = e;
-        ++_size;
+        _size++;
+        auto node = new Node<T>;
+        node->data = _data;
+        tail->next = node;
+        tail = node;
+        node->next = nullptr;
     }
 
-    // 出队
     void pop()
     {
-        if (_size == 0)
-            return; // 防止空队列操作
-        _front = (_front + 1) % _capacity;
-        --_size;
-        shrink();
+        _size--;
+        if (head->next == tail)
+        {
+            delete tail;
+            tail = head;
+        }
+        else
+        {
+            Node<T> *p = head->next->next;
+            delete head->next;
+            head->next = p;
+        }
     }
 
-    // 获取队列头部元素
-    T &front()
+    void clear()
     {
-        return _elem[_front];
+        for (int i = 0; i < _size; i++)
+            pop();
     }
 
-    T &back()
-    {
-        return _elem[(_front + _size - 1) % _capacity];
-    }
-
-    // 获取队列元素数量
-    int size() const
+    int size()
     {
         return _size;
     }
 
-    // 检查队列是否为空
-    bool empty() const
+    bool empty()
     {
         return _size == 0;
     }
 
-    // 析构函数
+    T &front()
+    {
+        return head->next->data;
+    }
+
     ~Queue()
     {
-        delete[] _elem;
+        clear();
+        delete head;
     }
 };
